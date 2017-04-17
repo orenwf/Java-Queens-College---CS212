31f41112f8735694b90cb43db85b1c48add896b0
Oren <ofriedman@acm.org>
2017-04-17 00:16:14 -0400

    Delete IllegalWordException.java

--- IllegalWordException.java
+++
@@ -1,6 +0,0 @@
-/** IllegalWordException throws an exception based on the Word class properties*/
-public class IllegalWordException extends IllegalArgumentException {
-    public IllegalWordException(String message) {
-        super(message);
-    }
-} 
